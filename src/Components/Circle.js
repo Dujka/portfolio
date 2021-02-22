b706dd5827682c761dfba11008e448b35861f3ff
aleksandar.dujkic <dujka97@gmail.com>
2021-02-22 23:50:27 +0100

    Added logo and changed circle gradient color and direction

--- src/Components/Circle.js
+++ src/Components/Circle.js
@@ -4,7 +4,7 @@ import CircleText from "./CircleText.js";
 
 function Circle() {
   return (
-    <div className="relative z-5 text-center block w-full pb-full rounded-full bg-gradient-to-b from-green-400 to-blue-400 justify-self-center col-span-6">
+    <div className="relative z-5 text-center block w-full pb-full rounded-full bg-gradient-to-br from-purple-400 to-blue-400 justify-self-center col-span-6">
       <ProfileImage />
       <CircleText />
     </div>
